71b9bb37c3a51eb608cdbd0e9243053ef588509b
lmReef <ReefMatson@gmail.com>
2024-06-13 01:19:37 +0100

    feat: first implementation of card placeholder stuff

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -85,16 +85,16 @@ export default function CustomSlider({
 
   return (
     <>
-      <div className="slider-container w-full">
-        <h2 className="slider-title text-2xl">{title}</h2>
-        <Slider className="p-4" {...sliderSettings}>
-          {data ? (
-            data.map((x, key) => <WikiCard data={x} key={key} />)
-          ) : (
-            <p>Loading...</p>
-          )}
-        </Slider>
-      </div>
+      {data && (
+        <div className="slider-container w-full">
+          <h2 className="slider-title text-2xl">{title}</h2>
+          <Slider className="p-4" {...sliderSettings}>
+            {data.map((x, key) => (
+              <WikiCard data={x} key={key} />
+            ))}
+          </Slider>
+        </div>
+      )}
     </>
   );
 }
--- components/WikiCard.tsx
+++ components/WikiCard.tsx
@@ -7,24 +7,30 @@ import {
 import { useRouter } from "next/navigation";
 import SaveButton from "./SaveButton";
 import styles from "./WikiCard.module.scss";
+import { useRef, useState } from "react";
 
 export default function WikiCard({ data }: { data: WikiData }) {
   const router = useRouter();
+  const [imageLoaded, setImageLoaded] = useState(!data.thumbnail);
 
   const searchHandler = () => {
-    if (router)
+    if (router && data)
       router.push(
         `/search/${encodeURIComponent(data.title || data.matched_title || "home")}`,
       );
   };
 
   const infoHandler = () => {
-    router.push(`/info/${data.title}`);
+    if (data) router.push(`/info/${data.title}`);
+  };
+
+  const imageLoadedHandler = () => {
+    setImageLoaded(true);
   };
 
   // TODO: probably move this out of this component and process all the data as it's received
   // because sometimes the responses are slightly different
-  if (data.thumbnail) {
+  if (data && data.thumbnail) {
     if (data.thumbnail.source) {
       data.thumbnail.url = data.thumbnail.source;
     }
@@ -36,43 +42,57 @@ export default function WikiCard({ data }: { data: WikiData }) {
     <div
       className={`wiki-card m-auto flex h-full max-h-full w-80 max-w-80 flex-shrink-0 flex-col gap-2 rounded bg-white bg-opacity-5 p-4 pb-0 text-center ${styles["wiki-card"]}`}
     >
-      <h3 className="overflow-hidden text-ellipsis whitespace-nowrap text-xl">
-        {data.title}
-      </h3>
+      {data && (
+        <>
+          <h3
+            className={`overflow-hidden text-ellipsis whitespace-nowrap text-xl transition-all ${imageLoaded || "animate-pulse rounded bg-gray-400 text-[rgba(0,0,0,0)] opacity-20"}`}
+          >
+            {data.title}
+          </h3>
 
-      <p className="flex flex-grow flex-col justify-center">
-        {data.description}
-      </p>
-      {data.thumbnail && (
-        <img
-          src={`${data.thumbnail.url}`}
-          className="h-56 flex-grow rounded-sm object-cover"
-          alt="thumbnail"
-        />
+          <p
+            className={`flex flex-grow flex-col justify-center transition-all ${imageLoaded || "animate-pulse rounded bg-gray-400 text-[rgba(0,0,0,0)] opacity-20"}`}
+          >
+            {data.description}
+          </p>
+
+          {data.thumbnail && (
+            <>
+              <img
+                onLoad={imageLoadedHandler}
+                src={`${data.thumbnail.url}`}
+                className={`h-56 flex-grow rounded-sm object-cover transition-all ${imageLoaded || "animate-pulse bg-gray-400 text-[rgba(0,0,0,0)] opacity-20"}`}
+                alt={"thumbnail"}
+              />
+            </>
+          )}
+
+          <div
+            className={`wiki-card-toolbar -mx-4 mt-2 flex pb-0 ${imageLoaded || "animate-pulse opacity-20"}`}
+          >
+            <Button
+              onClick={searchHandler}
+              className="flex-grow rounded-l rounded-r-none"
+              tooltip="Search"
+            >
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </Button>
+
+            <Button
+              onClick={infoHandler}
+              className="flex-grow rounded-l-none rounded-r-none"
+              tooltip="More Info"
+            >
+              <FontAwesomeIcon icon={faInfoCircle} className="text-lg" />
+            </Button>
+
+            <SaveButton
+              title={data.title}
+              className="flex-grow rounded-l-none rounded-r"
+            />
+          </div>
+        </>
       )}
-
-      <div className="wiki-card-toolbar -mx-4 mt-2 flex pb-0">
-        <Button
-          onClick={searchHandler}
-          className="flex-grow rounded-l rounded-r-none"
-          tooltip="Search"
-        >
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </Button>
-
-        <Button
-          onClick={infoHandler}
-          className="flex-grow rounded-l-none rounded-r-none"
-          tooltip="More Info"
-        >
-          <FontAwesomeIcon icon={faInfoCircle} className="text-lg" />
-        </Button>
-
-        <SaveButton
-          title={data.title}
-          className="flex-grow rounded-l-none rounded-r"
-        />
-      </div>
     </div>
   );
 }
